ec9804d6cceac0463bc875c20133819b72a58383
Lars Borchert <40208002+Lars314@users.noreply.github.com>
2019-05-24 14:30:07 -0700

    Set up part 1 file

---
+++ stats functions.py
@@ -0,0 +1,13 @@
+# we can put the code for our stats functions in here
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def main():
+    """
+    """
+
+
+if __name__ == '__main__':
+    main()
